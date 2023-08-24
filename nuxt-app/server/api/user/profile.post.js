e3b9e11ec5ffaa157885d6ffebc4f9425b32d33c
Andy Cogbill <acogbill@twilio.com>
2023-08-24 12:08:16 -0400

    Making Profile env variable

--- nuxt-app/server/api/user/profile.post.js
+++ nuxt-app/server/api/user/profile.post.js
@@ -2,20 +2,23 @@ export default defineEventHandler(async (event) => {
     const rawBody = await readBody(event)
 
     const userID = rawBody.userID
+
+    const tokenString = process.env.PROFILES_PREFIX_TO_USE + 'PROFILES_ACCESS_TOKEN'
+    const spaceIDString = process.env.PROFILES_PREFIX_TO_USE + 'PROFILES_SPACE_ID'
     
     const options = {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
             // NOTE the `:` after the Token in the below
-            'Authorization': `Basic ${Buffer.from(`${process.env.PROFILES_ACCESS_TOKEN}:`).toString('base64')}`,
+            'Authorization': `Basic ${Buffer.from(`${process.env[tokenString]}:`).toString('base64')}`,
         },
     }
 
     // Because the URL changes if sending an anonymous ID
     const idLabel = rawBody.anon === 'true' ? 'anonymous_id' : 'user_id'
     console.log('fetching profile for ID: ', userID)
-    const requestURL = `https://profiles.segment.com/v1/spaces/${process.env.PROFILES_SPACE_ID}/collections/users/profiles/${idLabel}:${userID}/traits`
+    const requestURL = `https://profiles.segment.com/v1/spaces/${process.env[spaceIDString]}/collections/users/profiles/${idLabel}:${userID}/traits`
 
     try {
             const fetchedProfile = await fetch(requestURL, options)
