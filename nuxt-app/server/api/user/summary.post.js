c702f2f3f275407c7914636a48a57a60097686e4
Andy Cogbill <acogbill@twilio.com>
2023-11-24 09:12:18 -0500

    Prompt improvement

--- nuxt-app/server/api/user/summary.post.js
+++ nuxt-app/server/api/user/summary.post.js
@@ -16,17 +16,18 @@ const AI = new OpenAI({
 });
 
 async function main(profileData) {
+
     const completion = await AI.chat.completions.create({
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant designed to output JSON. Starting with an initial object consisting of two variables and their values, 'traits' and 'events', you will summarize in two sentences everything the data describes about a human, which you'll refer to as 'This profile', whose traits describe their characteristics, and events describe their recent behaviors. Traits are provided in one object as name-value pairs; name is the type of characteristic written as a variable name (named with single String paraphrasing a natural description of a human, like 'Favorite Color' becomes 'favoriteColor' or 'favorite_color') and the value is what describes that specific human.  Events are an array of web page views and actions that the human takes, which can be initiated by the human (like 'Clicked Link', or 'Placed Order') or by a system about a company doing business with that human (for instance, a shipment tracking system would track the event of shipping an order to that human with 'Order Shipped' and save it to the array of events. Each event has a name and properties (which are also stored as variables, with single-string names similar to those of trait names described above) associated with it that provide further detail on the event. You should emphasize the importance of more recent events, which are closer to the beginning of the array, and deemphasize the importance of anything less intentful like a Home Page visit or 'Signed In'. Your response should be in the format of a JSON object with one property called 'summary'.",
-        },
-        { role: "user", content: JSON.stringify(profileData) },
-      ],
-      model: "gpt-4-1106-preview",//"gpt-3.5-turbo-1106",
-      response_format: { type: "json_object" },
-    });
+        messages: [
+          {
+            role: "system",
+            content: "Refactor a business customer's 'traits' (key-value pairs of human attributes) and 'events' (array of their interactions with the business; please emphasize recent/significant and deemphasize default or frequent activities like Home Page visits) into a JSON 'summary' with two prioritized sentences. The second sentence should suggest an action to a customer service agent to provide added value to the customer, while also increasing revenue for the business extracted from that customer.",
+          },
+          { role: "user", content: JSON.stringify(profileData) },
+        ],
+        model: "gpt-4-1106-preview",//"gpt-3.5-turbo-1106",
+        response_format: { type: "json_object" },
+      });
 
     console.log(`${completion.usage.prompt_tokens} OpenAI tokens used`)
 
