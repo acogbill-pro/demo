dad0f042c386f5de729625fb311ce6c6f3dfc32d
Andy Cogbill <acogbill@twilio.com>
2023-11-26 18:06:34 -0500

    Fancier exclusions handling

--- nuxt-app/server/api/user/events.post.js
+++ nuxt-app/server/api/user/events.post.js
@@ -17,8 +17,8 @@ export default defineEventHandler(async (event) => {
 
     // Because the URL changes if sending an anonymous ID
     const idLabel = isAnon === 'true' ? 'anonymous_id' : 'user_id'
-    
-    const requestURL = `https://profiles.segment.com/v1/spaces/${process.env[spaceIDString]}/collections/users/profiles/${idLabel}:${userID}/events?limit=100&exclude=Audience%20Entered%2CAudience%20Exited`
+    const exclusions = 'Audience Entered,Audience Exited,Trait Computed'
+    const requestURL = `https://profiles.segment.com/v1/spaces/${process.env[spaceIDString]}/collections/users/profiles/${idLabel}:${userID}/events?limit=100&exclude=${encodeURIComponent(exclusions)}`
     console.log('fetching events for ID: ', userID, requestURL)
     try {
             const fetchedProfile = await fetch(requestURL, options)
