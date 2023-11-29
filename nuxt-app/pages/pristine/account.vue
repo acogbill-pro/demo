97513229ec5390631d640bfdfffd4b83e6f9eac0
Andy Cogbill <acogbill@twilio.com>
2023-11-29 18:18:55 -0500

    Making Account page component shared

--- nuxt-app/pages/pristine/account.vue
+++ nuxt-app/pages/pristine/account.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { useAnalytics } from '~/stores/analytics.js'
-import { useProfileTraitsStore } from '~/stores/profileTraits.js'
 const analytics = useAnalytics()
-const profiles = useProfileTraitsStore()
 
 definePageMeta({
     layout: "default",
@@ -17,68 +15,10 @@ onMounted(() => {
 
     //profiles.persistUser()
 })
-
-const collapse = ref(true)
-
-function toggleCollapse() {
-    collapse.value = !collapse.value
-}
-
-const isLoggedIn = computed(() => {
-    return !analytics.bestIDIsAnonymous
-})
-
-const loginTitle = computed(() => {
-    return profiles.hasTraits ? `Logged in as ${profiles.traits.name}` : `Logged in`
-})
-
-function logout() {
-    profiles.unload()
-}
 </script>
 
 <template>
-    <v-container fluid>
-        <v-row v-if="!isLoggedIn || (isLoggedIn && !collapse)" class="bg">
-            <v-col>
-                <UserLogin />
-            </v-col>
-            <v-col>
-                <UserRegister />
-            </v-col>
-            `<v-col>
-                <SharedSidebar />
-            </v-col>`
-        </v-row>
-        <v-row v-else class="bg">
-            <v-col cols="2" />
-            <v-col cols="5">
-                <v-card>
-                    <v-card-title>Logged In</v-card-title>
-                    <v-card-text>
-
-                        <SharedUserSummary />
-                    </v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <SharedSidebar />
-            </v-col>`
-            <v-col cols="2" />
-        </v-row>
-        <v-row v-if="isLoggedIn" class="my-5">
-            <v-btn @click="logout()" class="mr-3">Logout</v-btn>
-            <v-btn v-if="!collapse" @click="toggleCollapse()" class="mr-3">Hide Login</v-btn>
-            <v-btn v-else @click="toggleCollapse" class="mr-3">Login Again</v-btn>
-        </v-row>
-    </v-container>
+    <SharedProfileAccount />>
 </template>
 
-<style lang="scss" scoped>
-.bg {
-    // background: url("/images/kfcBG.jpeg") no-repeat center center;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-}
-</style>
+<style lang="scss" scoped></style>
