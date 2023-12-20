0737b7693d14e4cab5009a97eb0a62569fabdd68
Andy Cogbill <acogbill@twilio.com>
2023-12-20 17:51:15 -0500

    adding in User Events for demo flow

--- nuxt-app/pages/pristine/products/cart.vue
+++ nuxt-app/pages/pristine/products/cart.vue
@@ -19,20 +19,30 @@ onMounted(() => {
 </script>
 
 <template>
-    <v-card>
-        <!-- <v-card-title>Shopping Cart</v-card-title> -->
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <!-- <v-card-title>Shopping Cart</v-card-title> -->
 
-        <v-card-text v-if="cart.totalQuantity > 0">
-            <PristineCartContents />
-        </v-card-text>
-        <v-card-text v-else>
-            Cart is Empty
-        </v-card-text>
-        <v-card-actions>
-            <v-btn to="/pristine/products" nuxt>Continue Shopping</v-btn>
-            <v-btn to="/pristine/products/checkout" nuxt>Checkout</v-btn>
-        </v-card-actions>
-    </v-card>
+                    <v-card-text v-if="cart.totalQuantity > 0">
+                        <PristineCartContents />
+                    </v-card-text>
+                    <v-card-text v-else>
+                        Cart is Empty
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn to="/pristine/products" nuxt>Continue Shopping</v-btn>
+                        <v-btn to="/pristine/products/checkout" nuxt>Checkout</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col cols="3"></v-col>
+            <v-col cols="4">
+                <SharedEventList />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <style lang="scss" scoped></style>
